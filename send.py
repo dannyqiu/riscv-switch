35f41c2f8620466f630616a310521fb81f5d69b0
Danny Qiu <dqiu55@gmail.com>
2018-11-26 04:06:51 -0500

    Update send.py script to encode instruction in RISCV format

--- send.py
+++ send.py
@@ -32,17 +32,6 @@ class ProtoWrapper(Packet):
     ]
 
 
-class Instruction(Packet):
-    name = 'Instruction'
-    fields_desc = [
-        BitField('opcode', 0, 6),
-        BitField('dst', 0, 5),
-        BitField('src', 0, 5),
-        BitField('target', 0, 5),
-        BitField('imm', 0, 11),
-    ]
-
-
 class Registers(Packet):
     name = 'Registers'
     fields_desc = [
@@ -77,154 +66,166 @@ class Registers(Packet):
         IntField('r28', 0),
         IntField('r29', 0),
         IntField('r30', 0),
-        IntField('pc', 0),
+        IntField('r31', 0),
     ]
 
 
-bind_layers(UDP, ProtoWrapper, dport=PROTOCOL_PORT)
-bind_layers(ProtoWrapper, Registers, type=PROTO_PROGRAM)
+class Instruction(Packet):
+    name = 'Instruction'
+    fields_desc = [
+        BitField('funct7', 0, 7),
+        BitField('part1', 0, 5),
+        BitField('part2', 0, 5),
+        BitField('funct3', 0, 3),
+        BitField('part3', 0, 5),
+        BitField('opcode', 0, 7),
+    ]
+
+
+bind_layers(IP, ProtoWrapper)
+bind_layers(ProtoWrapper, Registers)
 bind_layers(Registers, Instruction)
 bind_layers(Instruction, Instruction)
 
 
 def AddI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000000, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+    return Instruction(funct7=(imm >> 5), part1=((1 << 5 - 1) & imm), part2=src, funct3=0b000, part3=dst, opcode=0b0010011, **kwargs)
 
 
 def Add(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100000, dst=dst, src=src, target=target, **kwargs)
+    return Instruction(funct7=0b0000000, part1=target, part2=src, funct3=0b000, part3=dst, opcode=0b0110011, **kwargs)
 
 
-def SubI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000001, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def SubI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b000001, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Sub(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100001, dst=dst, src=src, target=target, **kwargs)
+# def Sub(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b100001, dst=dst, src=src, target=target, **kwargs)
 
 
-def MulI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000010, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def MulI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b000010, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Mul(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100010, dst=dst, src=src, target=target, **kwargs)
+# def Mul(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b100010, dst=dst, src=src, target=target, **kwargs)
 
 
-def DivI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000011, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def DivI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b000011, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Div(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100011, dst=dst, src=src, target=target, **kwargs)
+# def Div(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b100011, dst=dst, src=src, target=target, **kwargs)
 
 
-def SllI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000100, dst=dst, src=src, target=imm, **kwargs)
+# def SllI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b000100, dst=dst, src=src, target=imm, **kwargs)
 
 
-def Sll(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100100, dst=dst, src=src, target=target, **kwargs)
+# def Sll(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b100100, dst=dst, src=src, target=target, **kwargs)
 
 
-def SrlI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000110, dst=dst, src=src, target=imm, **kwargs)
+# def SrlI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b000110, dst=dst, src=src, target=imm, **kwargs)
 
 
-def Srl(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100110, dst=dst, src=src, target=target, **kwargs)
+# def Srl(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b100110, dst=dst, src=src, target=target, **kwargs)
 
 
-def SraI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b000111, dst=dst, src=src, target=imm, **kwargs)
+# def SraI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b000111, dst=dst, src=src, target=imm, **kwargs)
 
 
-def Sra(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b100111, dst=dst, src=src, target=target, **kwargs)
+# def Sra(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b100111, dst=dst, src=src, target=target, **kwargs)
 
 
-def AndI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b001000, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def AndI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b001000, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def And(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b101000, dst=dst, src=src, target=target, **kwargs)
+# def And(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b101000, dst=dst, src=src, target=target, **kwargs)
 
 
-def OrI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b001001, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def OrI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b001001, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Or(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b101001, dst=dst, src=src, target=target, **kwargs)
+# def Or(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b101001, dst=dst, src=src, target=target, **kwargs)
 
 
-def XorI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b001010, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def XorI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b001010, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Xor(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b101010, dst=dst, src=src, target=target, **kwargs)
+# def Xor(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b101010, dst=dst, src=src, target=target, **kwargs)
 
 
-def MovI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b001100, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def MovI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b001100, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Mov(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b101100, dst=dst, src=src, target=target, **kwargs)
+# def Mov(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b101100, dst=dst, src=src, target=target, **kwargs)
 
 
-def Movz(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b101101, dst=dst, src=src, target=target, **kwargs)
+# def Movz(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b101101, dst=dst, src=src, target=target, **kwargs)
 
 
-def Movn(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b101110, dst=dst, src=src, target=target, **kwargs)
+# def Movn(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b101110, dst=dst, src=src, target=target, **kwargs)
 
 
-def SwI(dst=0, src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b010000, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
+# def SwI(dst=0, src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b010000, dst=dst, src=src, target=(imm >> 11), imm=(0b11111111111 & imm), **kwargs)
 
 
-def Sw(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b110000, dst=dst, src=src, target=target, **kwargs)
+# def Sw(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b110000, dst=dst, src=src, target=target, **kwargs)
 
 
-def Lw(dst=0, src=0, target=0, **kwargs):
-    return Instruction(opcode=0b110001, dst=dst, src=src, target=target, **kwargs)
+# def Lw(dst=0, src=0, target=0, **kwargs):
+#     return Instruction(opcode=0b110001, dst=dst, src=src, target=target, **kwargs)
 
 
-def Beq(src=0, target=0, imm=0, **kwargs):
-    return Instruction(opcode=0b011000, src=src, target=target, imm=(0b11111111111 & imm), **kwargs)
+# def Beq(src=0, target=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b011000, src=src, target=target, imm=(0b11111111111 & imm), **kwargs)
 
 
-def Bne(src=0, target=0, imm=0, **kwargs):
-    return Instruction(opcode=0b011001, src=src, target=target, imm=(0b11111111111 & imm), **kwargs)
+# def Bne(src=0, target=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b011001, src=src, target=target, imm=(0b11111111111 & imm), **kwargs)
 
 
-def Bgez(src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b011010, src=src, imm=(0b11111111111 & imm), **kwargs)
+# def Bgez(src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b011010, src=src, imm=(0b11111111111 & imm), **kwargs)
 
 
-def Blez(src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b011011, src=src, imm=(0b11111111111 & imm), **kwargs)
+# def Blez(src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b011011, src=src, imm=(0b11111111111 & imm), **kwargs)
 
 
-def Bgtz(src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b011100, src=src, imm=(0b11111111111 & imm), **kwargs)
+# def Bgtz(src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b011100, src=src, imm=(0b11111111111 & imm), **kwargs)
 
 
-def Bltz(src=0, imm=0, **kwargs):
-    return Instruction(opcode=0b011101, src=src, imm=(0b11111111111 & imm), **kwargs)
+# def Bltz(src=0, imm=0, **kwargs):
+#     return Instruction(opcode=0b011101, src=src, imm=(0b11111111111 & imm), **kwargs)
 
 
-def EndOfProgram(src=0, imm=0, **kwargs):
-    return Instruction(opcode=-1, dst=-1, src=-1, target=-1, imm=-1, **kwargs)
+def EndOfProgram(**kwargs):
+    return Instruction(funct7=-1, part1=-1, part2=-1, funct3=-1, part3=-1, opcode=-1, **kwargs)
 
 
 def make_program(pkt, insns):
-    pkt /= ProtoWrapper(type=PROTO_PROGRAM, src=IP().src, max_steps=0xFFFF)
+    pkt /= ProtoWrapper(src=IP().src, pc=0, steps=0)
     pkt /= Registers()
     assert len(insns) < MAX_PROGRAM_LENGTH
     for insn in insns:
@@ -243,18 +244,17 @@ def main():
 
     pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
     pkt = pkt / IP(dst=addr)
-    pkt = pkt / UDP(sport=1234, dport=PROTOCOL_PORT)
     # Increase default recursion depth so scapy can handle longer programs
     sys.setrecursionlimit(10000)
     pkt = make_program(pkt, [
+        AddI(dst=3, src=0, imm=42),
         Add(dst=0, src=0, target=0),
-        AddI(dst=0, src=0, imm=42),
-        Sub(dst=0, src=0, target=0),
-        SubI(dst=0, src=0, imm=42),
-        Mul(dst=0, src=0, target=0),
-        MulI(dst=0, src=0, imm=42),
-        Div(dst=0, src=0, target=0),
-        DivI(dst=0, src=0, imm=42),
+        # Sub(dst=0, src=0, target=0),
+        # SubI(dst=0, src=0, imm=42),
+        # Mul(dst=0, src=0, target=0),
+        # MulI(dst=0, src=0, imm=42),
+        # Div(dst=0, src=0, target=0),
+        # DivI(dst=0, src=0, imm=42),
     ])
     hexdump(pkt)
     pkt.show2()
